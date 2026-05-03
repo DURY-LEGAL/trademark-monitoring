317e4a271642c4e8b94f9e09d2461e872cdbcb57
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:40:50 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-monitoring - Trademark monitoring - Automated brand protection and infringement detection. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
